47f0629e203a98fee2a6646a22155c768846f1d9
Thayana Vilaça Fortunato <76934028+thayana2021js@users.noreply.github.com>
2022-02-05 20:09:36 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ComandosDowhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean condicao = true;
+            int valor;
+
+            do{
+            Console.WriteLine("DIGITE UM VALOR, 0 PARA SAIR");
+            valor = int.Parse(Console.ReadLine());
+
+                if(valor ==0){
+                   Console.WriteLine(" Você saiu da aplicação ");
+                   condicao = false;
+                }
+                 else{
+                 Console.WriteLine("o valor informado é: "+ valor);
+
+                } 
+            }while(condicao == true);
+
+        }    
+    }
+}
